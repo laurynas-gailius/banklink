4c9519cabb0abca659822096eebbb6aa358262c4
Laurynas Gailius <LaurynasG@NB-314114.local>
2022-05-19 22:06:35 +0300

    positioning

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 let ul = document.querySelector("ul");
 let listItems = ul.getElementsByTagName("li");
+let form = document.getElementById("form");
+let loginMethods = document.getElementById("loginMethods");
+let recipient = document.getElementById("recipient");
+let heroCont = document.getElementById("heroCont");
+let otherInfo = document.getElementById("otherInfo");
 
 const selectedItem = ["selected", "text-black", "font-medium", "text-sm", "bg-gray-200", "py-1.5", "rounded-full", "px-3.5", "hover:cursor-auto"];
-const defaultItem = ["text-gray-500", "font-medium", "text-sm", "bg-white", "py-1.5", "rounded-full", "hover:text-black", "cursor-pointer"];
+const defaultItem = ["text-gray-500", "font-medium", "text-sm", "bg-white", "py-1.5", "px-3.5", "rounded-full", "hover:text-black", "cursor-pointer"];
 
-function loginMethods(){
+function loginMethodStyles(){
     for (let i = 0; i < listItems.length; i++){
         if(listItems[i].classList.contains('selected')){
             listItems[i].classList.add(...selectedItem);
@@ -21,6 +26,19 @@ ul.addEventListener("click", function(e) {
             listItems[i].classList.add(...defaultItem);
             e.target.className = '';
             e.target.classList.add(...selectedItem);
+            if(form.style.display === 'none'){
+                form.style.display = 'block';
+            }
+            if(otherInfo.style.display === 'block'){
+                otherInfo.style.display = 'none';
+            }
         }
+    }
+});
+
+ul.addEventListener("click", function(e) {
+    if(e.target === document.getElementById("otherMethods")) {
+        otherInfo.style.display = 'block';
+        form.style.display = 'none';
     }
 });
